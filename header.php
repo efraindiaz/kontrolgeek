bbb9e5936013c4152936c6b9ddc5d7f36ec2d134
efraindiaz <efraindiazcode@gmail.com>
2016-11-26 14:54:38 -0500

    Estructura

---
+++ header.php
@@ -0,0 +1,5 @@
+<div style="background-color:purple; width:100%; height:80px;">
+	<div style="width:250px; height:100%; display:inline-block;">
+		<img src="img/logo1.png" alt="" style="widht:250px; height:80%;">
+	</div>
+</div>
