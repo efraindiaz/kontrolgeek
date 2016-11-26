5563467acf1ae428426fbeb5df7ac7debf8a6348
efraindiaz <efraindiazcode@gmail.com>
2016-11-26 15:22:25 -0500

    plugins
    Bootstrap
    jquery

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<script src="js/jquery.min.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
+	<title>Document</title>
+</head>
+<body>
+	<?php require_once('header.php'); ?>
+	
+</body>
+</html>
