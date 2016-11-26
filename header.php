eac6db264ed1cbea8c00e229f85c1f20c25042f3
efraindiaz <efraindiazcode@gmail.com>
2016-11-26 15:05:11 -0500

    carpetas restantes

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<div style="background-color:purple; width:100%; height:80px;">
+<div style="background-color:#542682; width:100%; height:80px;">
 	<div style="width:250px; height:100%; display:inline-block;">
 		<img src="img/logo1.png" alt="" style="widht:250px; height:80%;">
 	</div>
