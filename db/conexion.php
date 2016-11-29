00014a5e3b0fd62d53ce6a17de8bbf5a91c2091d
efraindiaz <efraindiazcode@gmail.com>
2016-11-29 17:53:59 -0500

    Conexion
    conexion server

--- db/conexion.php
+++ db/conexion.php
@@ -1,10 +1,10 @@
 <?php 
 	function dbConexion(){
 		$conn =	null;
-	 	$host = 'localhost';
-	 	$db = 	'kontrolgeek';
-	 	$user = 'root';
-	 	$pwd = 	'';
+	 	$host = 'mysql.hostinger.mx';
+	 	$db = 	'u958544180_kgeek';
+	 	$user = 'u958544180_kgeek';
+	 	$pwd = 	'holamundo';
 		try {
 		   	$conn = new PDO('mysql:host='.$host.';dbname='.$db, $user, $pwd);
 		}
